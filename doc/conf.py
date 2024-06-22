a41690cab5343e0794460a17e94d71603f6a72d3
Joshua Li <joshuali925@gmail.com>
2024-06-22 00:33:49 +0000

    --wip--
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

--- doc/conf.py
+++ doc/conf.py
@@ -25,5 +25,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
